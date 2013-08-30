5cc9316212e3fd34ad11453b50175888cd2626fb
Jorge González <steinkel@gmail.com>
2013-08-29 23:24:00 -0700

    adding mocks and tests for Job model

--- Test/Case/Model/JobTest.php
+++ Test/Case/Model/JobTest.php
@@ -42,4 +42,45 @@ class JobTest extends CakeTestCase {
 		parent::tearDown();
 	}
 
+/**
+ * testTodayIsWorkday method
+ *
+ * @return void
+ */
+	public function testTodayIsWorkdaySuccess() {
+		$jobMock = $this->getMockForModel('Job', array('getNow'));
+		$jobMock->expects($this->once())
+			->method('getNow')
+			->will($this->returnValue(strtotime('next thursday')));
+		$result = $jobMock->todayIsWorkday(array());
+		$this->assertTrue($result, 'next thursday should be a workday');
+	}
+
+/**
+ * testTodayIsWorkday shall not pass
+ *
+ * @return void
+ */
+	public function testTodayIsWorkdayFail() {
+		$jobMock = $this->getMockForModel('Job', array('getNow'));
+		$jobMock->expects($this->once())
+			->method('getNow')
+			->will($this->returnValue(strtotime('next sunday')));
+		$result = $jobMock->todayIsWorkday(array());
+		$this->assertFalse($result, 'next sunday should NOT be a workday');
+	}
+	
+	public function testTodayIsWorkdaySuccessAndFail() {
+		$jobMock = $this->getMockForModel('Job', array('getNow'));
+		$jobMock->expects($this->at(0))
+			->method('getNow')
+			->will($this->returnValue(strtotime('next sunday')));
+		$jobMock->expects($this->at(1))
+			->method('getNow')
+			->will($this->returnValue(strtotime('next thursday')));
+		$result = $jobMock->todayIsWorkday(array());
+		$this->assertFalse($result, 'next sunday should NOT be a workday');
+		$result = $jobMock->todayIsWorkday(array());
+		$this->assertTrue($result, 'next thursday should NOT be a workday');
+	}
 }
